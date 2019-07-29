48f23f99c50322673b1a0bf946ecfb4dcebe83ce
Emre Çetin <emrec.tin@gmail.com>
2019-07-29 16:29:51 +0300

    add file copy and file write function

--- file.py
+++ file.py
@@ -1,9 +1,10 @@
+#### DOSYA OKUMA ####
+
 # Eğer dosya yoksa
 try:
     file = open("info.txt", "r")
 except FileNotFoundError:
     print("Dosya Bulunamadı")
-
 
 # file üzerinde satır satır gezme
 for i in file:
@@ -13,3 +14,20 @@ file.close()
 # Tüm satırları listeye alma
 liste = file.readlines()
 file.close()
+
+# Dosya okumanın kolay yolu open ve close fonksiyonlarını kullanamıza gerek kalmaz.
+# İlk 4 karakterin okuma, boş bırakılırsa tamamını okur.
+with open(example1, "r") as file1:
+    print(file1.read(4))
+   
+#### DOSYA YAZMA ####
+
+# Satır yazma
+with open('/resources/data/Example2.txt', 'w') as writefile:
+    writefile.write("This is line A")
+ 
+#### DOSYA KOPYALAMA ####
+with open('Example2.txt','r') as readfile:
+    with open('Example3.txt','w') as writefile:
+          for line in readfile:
+                writefile.write(line)
